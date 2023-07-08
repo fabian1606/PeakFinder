529468984763ccddd65372f6519d500f1953bc0b
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2023-07-08 14:13:22 +0200

    next app

--- web_register/pages/index.jsx
+++ web_register/pages/index.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
 
 export default function Index() {
   const [email, setEmail] = useState('');
@@ -16,16 +20,34 @@ export default function Index() {
   const handleRegister = () => {
     axios
     axios.post('http://localhost:3003/register', { email, password })
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+    .then(res => {
+    if(res.status != 200){
+      alert('Registration successful!');
+    } else {
+      alert('Registration failed.');
+    }
+  })
+    .catch(err => alert(err+" Registration failed. Possibly wrong email or Password, or User already exists."));
   };
 
   return (
-    <div>
-      <p>This is a page</p>
-      <input type="text" value={email} onChange={handleEmailChange} />
-      <input type="password" value={password} onChange={handlePasswordChange} />
-      <button onClick={handleRegister}>Register</button>
+    <div id='canvas' style={{margin:"15%"}}>
+      <div style={{display:"flex", justifyContent:"space-between", flexFlow:"column", alignItems:"center", height:"400px"}}>
+      <h2>Register for Peakfinder</h2>
+      <div style={{display:"flex", flexFlow:"column", justifyContent:"space-between", width:"25%"}}>
+      <p>Email</p>
+      <input type="text" value={email} onChange={handleEmailChange} style={{borderRadius: "15px"}} />
+      <p>Password</p>
+      <input type="password" value={password} onChange={handlePasswordChange} style={{borderRadius: "15px"}}/>
+      <button onClick={handleRegister} style={{marginTop:"20px", border:"none", borderRadius:"7px", backgroundColor:"#242227", color:"white"}}>Register</button>
+      </div>
+      <h4>Download our app here</h4>
+      <div>
+      <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+<Image src="/download.png" alt="appstore" width={350} height={50}/>
+      </Link>
+      </div>
+      </div>
     </div>
   );
 }
