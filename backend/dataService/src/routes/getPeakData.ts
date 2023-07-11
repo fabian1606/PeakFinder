fd7bed236bbcbc311246ce1989221435c5b47537
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2023-07-11 17:07:57 +0200

    -

--- backend/dataService/src/routes/getPeakData.ts
+++ backend/dataService/src/routes/getPeakData.ts
@@ -25,47 +25,43 @@ const router: Router = Router();
 router.get("/", (req: Request, res: Response): void => {
   const Data = req.body;
   const PeakId = Data.peakId;
+  let arrOfPeaks: { peakId: string; peakName: string }[] = [];
 
-  if(!PeakId){
+  if (!PeakId) {
     res.send("No PeakId given");
-  }
-  else{
-        
-        MsgModel.find({peakId: PeakId})
-          .then((msgs: DocumentType<Msg>[] | null): void => {
-            if (msgs) {
-              // Handle the found msgs
-              console.log(msgs);
-              PeakModel.find({peakId: PeakId})
-              .sort({timestamp: -1})
-              .limit(1)
-              .then((peak: DocumentType<Peak>[] | null): void => {
-                  if (peak) {
-                      // Handle the found msgs
-                      console.log(peak);
-                      const msgsVar = msgs.map((msg) => {
-                            return {
-                                msg: msg.message}
-                        });
-                        const PeakVar = {"name": peak[0].name,"id": peak[0].peakId,"avgVisitors": peak[0].averageVisitors}
-                        const data = {"peak": PeakVar, "msgs": msgsVar}
-                        res.send(data);
-                  } else {
-                      console.log("No peak found for this peak");
-                  }
-                  }
-              )
-              .catch((err: Error): void => {
-                  console.log("Error finding peak: " + err);
+  } else {
+    MsgModel.find({ peakId: PeakId })
+      .then((msgs: DocumentType<Msg>[] | null): void => {
+        if (msgs) {
+          // Handle the found msgs
+          console.log(msgs);
+
+          PeakModel.find({ peakId: PeakId })
+            .then((peak: DocumentType<Peak>[] | null): void => {
+              if (peak) {
+                // Handle the found msgs
+                const msgsVar = msgs.map((msg) => {
+                  return {
+                    msg: msg.message,
+                  };
+                });
+                // const PeakVar = {"name": peak[0].name,"id": peak[0].peakId,"avgVisitors": peak[0].averageVisitors}
+                const data = { peak: peak, msgs: msgsVar };
+                res.send(data);
+              } else {
+                console.log("No peak found for this peak");
               }
-              );
-            } else {
-              console.log("No msgs found for this peak");
-            }
-          })
-          .catch((err: Error): void => {
-            console.log("Error finding msgs: " + err);
-          });
+            })
+            .catch((err: Error): void => {
+              console.log("Error finding peak: " + err);
+            });
+        } else {
+          console.log("No msgs found for this peak");
+        }
+      })
+      .catch((err: Error): void => {
+        console.log("Error finding msgs: " + err);
+      });
   }
 });
 
